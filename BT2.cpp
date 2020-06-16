cc8c340beeee2230b7d3498fc7709f859717f5d9
Thao Nhien <nhientruong.nt@gmail.com>
2020-06-16 14:18:24 +0900

    Lam xong bai tap 2

---
+++ BT2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Print a welcome text in a separate line:" << endl;
+    cout << "----------------------------------------" << endl;
+    cout << "Welcome to C++" << endl;
+    return 0;
+}
