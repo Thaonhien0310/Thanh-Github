5ee50fb1a2f09763645a852849d86f64a2b110ba
Thao Nhien <nhientruong.nt@gmail.com>
2020-06-16 14:42:54 +0900

    Hoan thanh bai tap 4

---
+++ BT4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 29;
+    int b = 30;
+    int sum;
+    sum = a + b;
+    cout << "Print the sum of two numbers :" << endl;
+    cout << "----------------------------------------" << endl;
+    // cout << "The sum of 29 and 30 is : " << a + b << endl;
+    cout << "The sum of 29 and 30 is : " << sum << endl;
+    return 0;
+}
